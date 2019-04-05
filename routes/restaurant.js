482679c7677cb801fb23a4a603abf6844df8d657
Matus Minarovic <matusm44@gmail.com>
2019-04-05 17:36:10 +0100

    Added call to get all tables by restaurant id

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,24 +3,33 @@ const mongo = require('../database/mongo.js');
 
 var router = express.Router();
 
-// call '/restaurant'
+// -------------------------- GET requests --------------------
+
+// call GET '/restaurant'
 router.get('/', function(req, res) {
     res.render('index', { title: '/restaurant' });
 });
 
-// call '/restaurant/:id'
+// call GET '/restaurant/:id'
 router.get('/:id', getRestaurantById);
 
-// call '/restaurant/:id/categories'
+// call GET '/restaurant/:id/categories'
 router.get('/:id/categories', getCategoriesByRestaurantId);
+
+// call GET '/restaurant/:id/tables'
+router.get('/:id/tables', getTablesByRestaurantId);
+
+
+// ------------------------ POST requests ---------------------
+
+// call POST '/restaurant'
+router.post('/', insertNewRestaurant);
+
+
+// ---------------------- Functions needed --------------------
 
 async function getRestaurantById(req, res) {
     const id = req.params.id;
-
-    // Establish connection
-    if (!mongo.isConnected())
-        await mongo.connectToMongo();
-
     const restaurant = await mongo.findOneById(id, 'restaurant');
 
     // Validating the restaurant id
@@ -35,11 +44,6 @@ async function getRestaurantById(req, res) {
 
 async function getCategoriesByRestaurantId(req, res) {
     const id = req.params.id;
-
-    // Establish connection
-    if (!mongo.isConnected())
-        await mongo.connectToMongo();
-
     const restaurant = await mongo.findOneById(id, 'restaurant');
 
     // Validating the restaurant id
@@ -57,6 +61,40 @@ async function getCategoriesByRestaurantId(req, res) {
         console.log('there is no restaurant with this id: ' + id);
         res.status(404).send();
     }
+}
+
+async function getTablesByRestaurantId(req, res) {
+    const id = req.params.id;
+    const restaurant = await mongo.findOneById(id, 'restaurant');
+
+    // Validating the restaurant id
+    if (restaurant) {
+        const tables = await mongo.findAllByRestaurantId(id, 'table');
+        if (tables) {
+            console.log('found restaurant: ' + JSON.stringify(restaurant));
+            console.log('found tables: ' + tables.toString());
+            res.send(tables);
+        } else {
+            console.log('there are no categories in this restaurant: ' + JSON.stringify(restaurant));
+            res.status(404).send();
+        }
+    } else {
+        console.log('there is no restaurant with this id: ' + id);
+        res.status(404).send();
+    }
+}
+
+async function insertNewRestaurant(req, res) {
+    const restaurant = req.body;
+
+    console.log(JSON.stringify(restaurant));
+
+    const result = await mongo.insertRestaurant(restaurant);
+
+    console.log(result);
+
+    res.json('User added successfully');
+    // res.status(400).send("unable to save to database");
 }
 
 module.exports = router;
